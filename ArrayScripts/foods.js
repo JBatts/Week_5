ba680ecacf34b72b0292289c322d73bcf89fccad
Jalen Battle <jalenbattle78@gmail.com>
2024-10-21 13:51:42 -0400

    food.js work

--- ArrayScripts/foods.js
+++ ArrayScripts/foods.js
@@ -1,10 +1,20 @@
-let lunch = {
-    "Steak Fajitas" : 9.95,
-    "Chips and Guacamole" : 5.25,
-    "Sweet Tea" : 2.79,
-    
-}
+let lunch = [
+    {item: "Steak Fajitas", price: 9.95},
+    {item: "Chips and Guacamole", price: 5.25},
+    {item: "Sweet Tea", price: 2.79}
+    ];
 
 function getTotal (event) {
-    let subtotal = 0
-}
+    let subtotal = 0;
+    for(i=0; i < event.length; i++) {
+        subtotal += event[i]["price"];
+    };
+    let tax = .08;
+    let tip = .18;
+    let totalCost = subtotal + (subtotal * tax) + (subtotal * tip);
+    return totalCost.toFixed(2);
+};
+console.log(getTotal(lunch));
+
+// mathF = 17.99 + 1.4392 + 3.2382
+// console.log(mathF) // Checking my math
